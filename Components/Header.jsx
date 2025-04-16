1dd392128fd094abff003898abc7178c7ad8a629
Anshuman1313 <168716686+Anshuman1313@users.noreply.github.com>
2025-04-16 20:56:36 +0530

    Update Header.jsx
    removed some unused comments

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -51,7 +51,7 @@ const Header = () => {
        </Link> 
       </div> */}
       
-      {/* //yis is the V0 version  */}
+    
       <div className="relative py-16 px-4 overflow-hidden">
       {/* Decorative elements */}
       <div className="absolute inset-0 -z-10 opacity-5">
@@ -105,21 +105,7 @@ const Header = () => {
           </p>
         </div>
       </div>
-    </div>
-  
-      {/* original */}
-     {/* <div className='text-center my-8 mt-[80px]'>
-
-         <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
-        <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p> 
-        <form onSubmit={onSubmitHandler} className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' action="">
-            <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder='Enter your email' className='pl-4 outline-none' required />
-            <button type='submit' className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white'>Subscribe</button>
-        </form>
-      </div>  */}
-
-
-      
+    </div>  
     </div>
 
 
